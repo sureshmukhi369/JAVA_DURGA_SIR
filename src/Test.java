fd1e4413769ff6868901f76df5c837f89ee6a8db
Suresh Mukhi <sureshmukhi098@gmail.com>
2025-04-12 14:19:10 +0530

    added identifiers

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/* -> Identifiers: A name in java program is called an identifier. which can used for identification purposes.
+*
+*
+*
+* */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("I am a billionare, yes");
+    }
+}
