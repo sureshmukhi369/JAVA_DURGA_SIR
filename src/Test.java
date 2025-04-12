4cdd65c19fdf85b7fea3beb6bc986f6631363c3b
Suresh Mukhi <sureshmukhi098@gmail.com>
2025-04-12 16:26:49 +0530

    Addedd investment earning line

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,8 @@
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("I am a billionare, yes");
+        System.out.println("I am a billionaire, yes");
+        System.out.println("I am earning 5 lacs a month, through investments");
+
     }
 }
