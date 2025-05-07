7e23dba322d98bbb723fcdfcb129ea317d27ea92
Suresh Mukhi <sureshmukhi098@gmail.com>
2025-05-07 06:10:10 +0530

    learning the Identifiers and Reserved Words

---
+++ src/Identifiers/Test.java
@@ -0,0 +1,25 @@
+package Identifiers;/* -> Identifiers: A name in java program is called an identifier. which can used for identification purposes.
+*  -> There is no length limit for java identifiers, but it is recommended too lengthy identifiers.
+*  -> we can't used reserved words as an identifiers.
+*   -> All predefined java class names and interface names we can use as indentifiers.
+*  Q) which of the following are java identifiers?
+* Ans:- total_number(v), total#,123total, total123(v), ca$h(v), _$_$_$_$(v), all@hands, java2share(v), Integer(v), Int(v), int
+*
+* */
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 10;
+        int String = 888;
+        int Runnable = 99;
+        System.out.println(x);
+        System.out.println(String);
+        System.out.println(Runnable);
+
+        System.out.println("I am a billionaire, yes");
+        System.out.println("I am earning 5 lacs a month, through investments");
+        System.out.println("I am a billionaire, from stock market i am a student of warren buffet.");
+        System.out.println("I am working as a java AI Engineer in DUBAI: ");
+
+    }
+}
---
+++ src/Reserved_Words/A.java
@@ -0,0 +1,12 @@
+package Reserved_Words;
+/* ->
+*
+*
+*
+*
+* */
+public class A {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
