858c3df2586378e0fc58bae4cbe19e78fad97cf0
Suresh Mukhi <sureshmukhi098@gmail.com>
2025-05-19 21:41:02 +0530

    added for my idol Warren Buffet , I am a billionaire.

--- src/Test.java
+++ src/Test.java
@@ -9,6 +9,8 @@ public class Test {
         System.out.println("I am a billionaire, yes");
         System.out.println("I am earning 5 lakhs a month, through investments");
         System.out.println("I am earning 1 crore only from dividends");
-
+        System.out.println("I am going with Parents to Dubai");
+        System.out.println("I am a true student of Warren Buffet");
+        System.out.println("After 10 years I am a billionaire person");
     }
 }
