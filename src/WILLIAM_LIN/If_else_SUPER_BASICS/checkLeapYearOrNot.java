fc1a74398e4fa894480cd4ce7d8b37eb51cb63ad
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-09-16 21:44:03 +0530

    modified check leap year

--- src/WILLIAM_LIN/If_else_SUPER_BASICS/checkLeapYearOrNot.java
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS/checkLeapYearOrNot.java
@@ -6,7 +6,7 @@ public class checkLeapYearOrNot {
     public static void main(String[] args) {
         System.out.print("Enter a year: ");
         int year = new Scanner(System.in).nextInt();
-        System.out.println((year % 4 == 0 || year % 400 == 0 || year % 100 != 0)
-                ? year + " is a leap year." : year + "is not a leap year.");
+        System.out.println((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+                ? year + " is a leap year." : year + " is not a leap year.");
     }
 }
