3404579e404533dbe734b556a5aea894cd397db4
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-09-18 07:34:22 +0530

    check divisible by 2 or 3

---
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/checkDivisibleBy2Or3.java
@@ -0,0 +1,13 @@
+package WILLIAM_LIN.If_else_SUPER_BASICS_50;
+
+import java.util.Scanner;
+public class checkDivisibleBy2Or3 {
+    public static void main(String[] args) {
+        System.out.print("Enter your number: ");
+        int num = new Scanner(System.in).nextInt();
+
+        System.out.println((num % 2 != 0 || num % 3 != 0) ? num + " is not divisible by 2 or 3" :
+                num + " is divisible by both 2 or 3");
+
+    }
+}
