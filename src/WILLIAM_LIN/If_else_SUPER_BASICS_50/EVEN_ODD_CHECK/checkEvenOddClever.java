ed73ff980782db030e969539f2e90c2da36ba362
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-09-22 19:45:54 +0530

    check even odd in short/clever ternary operator style

--- src/WILLIAM_LIN/If_else_SUPER_BASICS_50/EVEN_ODD_CHECK/checkEvenOddClever.java
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/EVEN_ODD_CHECK/checkEvenOddClever.java
@@ -1,6 +1,7 @@
 package WILLIAM_LIN.If_else_SUPER_BASICS_50.EVEN_ODD_CHECK;
 
 import java.util.Scanner;
+
 public class checkEvenOddClever {
     public static void main(String[] args) {
     System.out.print("Enter your number: ");
