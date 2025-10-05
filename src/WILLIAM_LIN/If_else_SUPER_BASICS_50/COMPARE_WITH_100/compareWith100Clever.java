731b86fc619795f010a0982090f9ed51a7a8b7b1
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-06 03:18:34 +0530

    compare with 100 using ternary operator short/clever

--- src/WILLIAM_LIN/If_else_SUPER_BASICS_50/COMPARE_WITH_100/compareWith100Clever.java
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/COMPARE_WITH_100/compareWith100Clever.java
@@ -1,2 +1,14 @@
-package WILLIAM_LIN.If_else_SUPER_BASICS_50.COMPARE_WITH_100;public class compareWith100Clever {
+package WILLIAM_LIN.If_else_SUPER_BASICS_50.COMPARE_WITH_100;
+
+import java.util.Scanner;
+public class compareWith100Clever {
+    public static void main(String[] args) {
+        System.out.println("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println((num > 100) ? (num + " is greater than 100") :
+                (num == 100) ? (num + " is equal to 100") :
+                        (num + " is less than 100"));
+    }
 }
