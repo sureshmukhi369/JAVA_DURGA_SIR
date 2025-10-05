4998e1055d5c87c7283a454f188829b25b1edd7b
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-06 02:13:41 +0530

    check the number is in the range or out of range 10 to 100 using ternary operator short/clever

---
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/RANGE_CHECK/rangeCheckClever.java
@@ -0,0 +1,14 @@
+package WILLIAM_LIN.If_else_SUPER_BASICS_50.RANGE_CHECK;
+
+import java.util.Scanner;
+public class rangeCheckClever {
+    public static void main(String[] args) {
+        System.out.print("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println((num >= 10 && num <= 100) ? num + " is in the range of 10 to 100" :
+                num + " is out of range");
+
+    }
+}
