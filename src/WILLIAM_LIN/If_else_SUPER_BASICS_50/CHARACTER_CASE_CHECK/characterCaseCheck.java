5895ab13cfbbfe237d16f0e0c643d7099e3da810
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-06 03:46:17 +0530

    character check

---
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/CHARACTER_CASE_CHECK/characterCaseCheck.java
@@ -0,0 +1,11 @@
+package WILLIAM_LIN.If_else_SUPER_BASICS_50.CHARACTER_CASE_CHECK;
+
+public class characterCaseCheck {
+    public static void main(String[] args) {
+
+    }
+
+    public static String characterCaseChecker(char ch) {
+
+    }
+}
