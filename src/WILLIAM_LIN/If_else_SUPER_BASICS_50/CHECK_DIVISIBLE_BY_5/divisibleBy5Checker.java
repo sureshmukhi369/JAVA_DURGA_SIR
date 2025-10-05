a10b037a0d495dda8e26a970ca23148ec79aa560
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-05 21:26:16 +0530

    check a number is divisible by 5 or not using method Industry style

---
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/CHECK_DIVISIBLE_BY_5/divisibleBy5Checker.java
@@ -0,0 +1,16 @@
+package WILLIAM_LIN.If_else_SUPER_BASICS_50.CHECK_DIVISIBLE_BY_5;
+
+import java.util.Scanner;
+public class divisibleBy5Checker {
+    public static void main(String[] args) {
+        System.out.print("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(divisibleBy5(num));
+    }
+
+    public static String divisibleBy5(int num) {
+        if (num % 5 == 0) return num + " is divisible by 5.";
+        else return num + " is not divisible by 5.";
+    }
+}
