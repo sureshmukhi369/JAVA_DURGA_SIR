dc1ac72f6b76fff2bb8bc3a67f98b5c96d194021
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-06 02:07:40 +0530

    check the number is range or out of range 10 to 100 using simple if else

---
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/RANGE_CHECK/rangeCheck.java
@@ -0,0 +1,16 @@
+package WILLIAM_LIN.If_else_SUPER_BASICS_50.RANGE_CHECK;
+
+import java.util.Scanner;
+public class rangeCheck {
+    public static void main(String[] args) {
+        System.out.print("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num >= 10 && num >= 100) {
+            System.out.println(num + " is the range of 10 to 100");
+        } else {
+            System.out.println(num + " is out of range");
+        }
+    }
+}
