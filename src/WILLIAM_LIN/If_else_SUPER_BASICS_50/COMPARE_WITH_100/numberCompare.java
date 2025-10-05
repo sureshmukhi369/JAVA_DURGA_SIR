c4d1294b5580a351cf76524cd93136aec9ffd973
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-06 03:19:33 +0530

    compare with 100 using method industry style

--- src/WILLIAM_LIN/If_else_SUPER_BASICS_50/COMPARE_WITH_100/numberCompare.java
+++ src/WILLIAM_LIN/If_else_SUPER_BASICS_50/COMPARE_WITH_100/numberCompare.java
@@ -1,2 +1,17 @@
-package WILLIAM_LIN.If_else_SUPER_BASICS_50.COMPARE_WITH_100;public class numberCompare {
+package WILLIAM_LIN.If_else_SUPER_BASICS_50.COMPARE_WITH_100;
+
+import java.util.Scanner;
+public class numberCompare {
+    public static void main(String[] args) {
+        System.out.print("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(compareNumber(num));
+    }
+
+    public static String compareNumber(int num) {
+        if (num > 100) return num + " is greater than 100";
+        else if (num == 100) return num + " is equal to 100";
+        else return num + " is less than 100";
+    }
 }
