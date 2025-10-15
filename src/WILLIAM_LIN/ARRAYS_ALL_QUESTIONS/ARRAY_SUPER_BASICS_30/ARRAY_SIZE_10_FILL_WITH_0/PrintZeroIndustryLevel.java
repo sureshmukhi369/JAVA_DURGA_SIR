08fc5efbb165b6c59eb31bbb7d9192ba5f23eb5e
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-15 14:31:38 +0530

    print all 0 using method way

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/ARRAY_SIZE_10_FILL_WITH_0/PrintZeroIndustryLevel.java
@@ -0,0 +1,14 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30.ARRAY_SIZE_10_FILL_WITH_0;
+
+public class PrintZeroIndustryLevel {
+    public static void main(String[] args) {
+        printZero(10);
+    }
+
+    public static void printZero(int arraySize) {
+int[] numbers = new int[arraySize];
+          for(int num : numbers) {
+              System.out.println(num + " ");
+          }
+    }
+}
