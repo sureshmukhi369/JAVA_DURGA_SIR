0c19dd888d03220c8b4fed9bf67d07982aa0d0e1
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-15 10:48:03 +0530

    print an Array industry style separated method

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/PrintArrayElementsIndustryStyle.java
@@ -0,0 +1,14 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30;
+
+public class PrintArrayElementsIndustryStyle {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for(int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
