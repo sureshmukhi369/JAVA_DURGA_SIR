a5b458b8fde4067c35da382a69e83cbbed4fed91
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-15 14:14:21 +0530

    print 0 times size of the array is 10 using for each loop

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/ARRAY_SIZE_10_FILL_WITH_0/PrintZeroArrayClever.java
@@ -0,0 +1,11 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30.ARRAY_SIZE_10_FILL_WITH_0;
+
+public class PrintZeroArrayClever {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+        for(int num : numbers) {
+            System.out.println(num + " ");
+        }
+    }
+}
