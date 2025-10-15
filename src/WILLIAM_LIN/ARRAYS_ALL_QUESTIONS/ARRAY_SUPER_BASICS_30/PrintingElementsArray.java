c6de4c3be5a6c16bfdc7803e66d08915a2e3f5e9
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-15 10:41:26 +0530

    printing an array in for each loop short and clever

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/PrintingElementsArray.java
@@ -0,0 +1,11 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30;
+
+public class PrintingElementsArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        for(int num : arr) {
+         System.out.println(num);
+        }
+    }
+}
