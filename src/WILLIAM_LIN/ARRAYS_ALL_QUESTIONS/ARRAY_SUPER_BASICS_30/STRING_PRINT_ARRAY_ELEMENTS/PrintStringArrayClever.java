5203d3422e6aef59ed3415762c3beef0785f54db
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-15 13:13:04 +0530

    print string using for each loop short and clever way

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/STRING_PRINT_ARRAY_ELEMENTS/PrintStringArrayClever.java
@@ -0,0 +1,12 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30.STRING_PRINT_ARRAY_ELEMENTS;
+
+public class PrintStringArrayClever {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
+
+        // Print each element in the array using enhanced for loop
+         for (String fruit : fruits) {
+             System.out.println(fruit + " is good for health");
+         }
+    }
+}
