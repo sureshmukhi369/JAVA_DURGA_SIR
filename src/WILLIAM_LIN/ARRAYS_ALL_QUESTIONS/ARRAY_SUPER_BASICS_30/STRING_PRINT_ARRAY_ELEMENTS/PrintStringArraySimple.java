db042ecc6d6b33d24c70fea9e5241e748a2bc824
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-15 13:03:55 +0530

    print string array using simple for loop

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/STRING_PRINT_ARRAY_ELEMENTS/PrintStringArraySimple.java
@@ -0,0 +1,12 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30.STRING_PRINT_ARRAY_ELEMENTS;
+
+public class PrintStringArraySimple {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
+
+        // Print each element in the array
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+    }
+}
