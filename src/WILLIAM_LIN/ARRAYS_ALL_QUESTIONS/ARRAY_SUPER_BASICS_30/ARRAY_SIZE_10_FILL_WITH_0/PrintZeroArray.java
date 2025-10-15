a89c955ec9fefe4d09a0e70a2f401bba9d39444f
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-15 13:56:26 +0530

    pring array size 10 print all elements 0 using for loop

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/ARRAY_SIZE_10_FILL_WITH_0/PrintZeroArray.java
@@ -0,0 +1,11 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30.ARRAY_SIZE_10_FILL_WITH_0;
+
+public class PrintZeroArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
