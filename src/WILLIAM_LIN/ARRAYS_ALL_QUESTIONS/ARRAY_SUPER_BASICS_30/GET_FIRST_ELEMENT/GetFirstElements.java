dbf903f27f5ecae1af5a50130bc9d00234fa5e9e
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-10-16 12:10:42 +0530

    get first element of an arrray

---
+++ src/WILLIAM_LIN/ARRAYS_ALL_QUESTIONS/ARRAY_SUPER_BASICS_30/GET_FIRST_ELEMENT/GetFirstElements.java
@@ -0,0 +1,8 @@
+package WILLIAM_LIN.ARRAYS_ALL_QUESTIONS.ARRAY_SUPER_BASICS_30.GET_FIRST_ELEMENT;
+
+public class GetFirstElements {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("First element: " + numbers[0]);
+    }
+}
